ee70c78e7b73799e08f1e099e8e5de70989914a2
mcalipster <aridwanrifani@gmail.com>
2020-02-14 15:05:26 +0700

    soal nomor 3

--- ujian.py
+++ ujian.py
@@ -1,33 +1,33 @@
 # 1. UraiRajutKata
-class UraiRajutKata:
-    def urai(self, kata):
-        self.kata = kata
-        hasil = ''
-        for s in range(len(kata)):
-            hasil += kata[:s+1]
-        return hasil
+# class UraiRajutKata:
+#     def urai(self, kata):
+#         self.kata = kata
+#         hasil = ''
+#         for s in range(len(kata)):
+#             hasil += kata[:s+1]
+#         return hasil
 
-    def Rajut(self, kata):
-        self.kata = kata
-        hasil = ''
-        i = 2
-        for a in range(len(kata)):
-            if kata[a] == kata[a]:
-                hasil += kata[:a+1]
-                continue
-            if kata[a+1] == kata[a+1]:
-                hasil += kata[:a+1]
-                continue
-            if kata[a+2] == kata[a+2]:
-                hasil += kata[:a+1]
-                continue
-            if kata[a+3] == kata[a+3]:
-                hasil += kata[:a+1]
-                continue
-x = UraiRajutKata()
-print(x.urai('Code'))
-print(x.urai('Python'))
-print(x.urai('Purwadhika'))
+#     def Rajut(self, kata):
+#         self.kata = kata
+#         hasil = ''
+#         i = 2
+#         for a in range(len(kata)):
+#             if kata[a] == kata[a]:
+#                 hasil += kata[:a+1]
+#                 continue
+#             if kata[a+1] == kata[a+1]:
+#                 hasil += kata[:a+1]
+#                 continue
+#             if kata[a+2] == kata[a+2]:
+#                 hasil += kata[:a+1]
+#                 continue
+#             if kata[a+3] == kata[a+3]:
+#                 hasil += kata[:a+1]
+#                 continue
+# x = UraiRajutKata()
+# print(x.urai('Code'))
+# print(x.urai('Python'))
+# print(x.urai('Purwadhika'))
 
 # 2. Segitiga Excel
 
@@ -52,3 +52,21 @@ print(x.urai('Purwadhika'))
 #         b += 1
 #     z += '\n'
 # print(z)
+
+# 3. API
+
+
+import requests
+#akses GET API http://raw.githubusercontent.com/LintangWisesa/Ujian_Fundamental_JCDS08/master/data/provinsi.json #"36" : "BANTEN"   #"32" : "JAWA BARAT" 
+
+# apikey = 'CLEnneF0dT2kb0LzVv0dOmUWKdBGU5KoXaCsD3zbWFCClulFRX3weYiOEaUudnGf'
+url = 'http://raw.githubusercontent.com/LintangWisesa/Ujian_Fundamental_JCDS08/master/data/provinsi.json'
+# cari = input('ketik nama provinsi: ')
+
+# headers = {
+#     'user-key': apikey
+# }
+
+data = requests.get(url)
+kodeprov = data.json()
+print(kodeprov)
